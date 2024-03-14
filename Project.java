d9332d069bd433dbc9e003f2a05e1e21ae757fb4
KaterinaDemianik <k.demianik12@gmail.com>
2024-03-14 01:33:23 -0700

    Adding scanner and using idea with them

--- Project.java
+++ Project.java
@@ -1,2 +1,9 @@
-public class Project {
+import java.util.Scanner;
+public class Project{
+    public static void main(String [] args){  
+        Scanner scanner=new Scanner(System.in);
+        String input=scanner.nextLine();
+        String [] divideNumbersString=input.split(" ");
+        String binaryStringSave="";
 }
+} 
