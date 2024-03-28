fc6b1e8a2e8202b68741f10475c6c24ab70f83ac
KaterinaDemianik <k.demianik12@gmail.com>
2024-03-28 02:00:13 -0700

    Add convert reading from stidn decimal to binary

--- Project.asm
+++ Project.asm
@@ -47,6 +47,83 @@ read_next:
     ;повторення циклу зчитування якщо кінець файлу ще не досягнутий, то програма продовжує зчитувати символи з стандартного ввдоду 
 
 
+decimal_to_binary: 
+	.CODE 
+		MAIN PROC FAR 
+			MOV AX, 
+	@DATA 
+		MOV DS, 
+	AX 
+
+;load the value stored; 
+in variable d1 
+	mov ax, 
+	d1 
+
+;convert the value to binary; 
+print the value 
+	CALL PRINT 
+
+;interrupt to exit
+	MOV AH, 
+	4CH INT 21H 
+
+	MAIN ENDP 
+		PRINT PROC 
+
+;initialize count 
+	mov cx, 
+	0 mov dx, 0 label1:; 
+if
+	ax is zero 
+		cmp ax, 
+		0 je print1 
+
+;initialize bx to 2 mov bx, 2 
+
+;divide it by 2 
+;to convert it to binary 
+	div bx 
+
+;push it in the stack 
+	push dx 
+
+;increment the count 
+	inc cx 
+
+;set dx to 0 
+	xor dx, 
+	dx 
+		jmp label1 
+			print1: 
+
+;check if count 
+;is greater than zero 
+	cmp cx, 
+	0 je exit
+
+;pop the top of stack 
+	pop dx 
+
+;add 48 so that it 
+;represents the ASCII 
+;value of digits 
+	add dx, 
+	48 
+
+;interrupt to print a 
+;character 
+	mov ah, 
+	02h int 21h 
+
+;decrease the count 
+	dec cx 
+		jmp print1 
+			exit : ret 
+					PRINT ENDP 
+						END MAIN 
+
+
 ;сортування масиву слів:
 bubble_sort: 
 array DW 3, 2, 6, 4, 1 
@@ -86,8 +163,6 @@ nextStep:
     loop outerLoop
 ;повторення зовнішнього циклу, але це для к-ті елементів в масиві -1
 
-
-decimal_to_binary: 
 
 
 calculate_median: 
